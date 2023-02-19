6984763df1adc371c9214698009318a6ae314f62
Huazifa-Ahmad <ahuzaifa64@gmail.com>
2023-02-19 23:18:24 +0500

    commit changes

--- client/src/view/Aboutus.js
+++ client/src/view/Aboutus.js
@@ -1,14 +1,26 @@
-import React from 'react'
-import '../style/Aboutus.css'
+import React from 'react';
+import '../style/Aboutus.css';
+import aboutusbanner from '../images/Banners/aboutusbanner.jpg';
 
 function Aboutus() {
   return (
     <div>
-      <main>
-        About Us
-      </main>
+      <div className='aboutusbanner'>
+        <img src={aboutusbanner} alt="aboutusbanner" />
+      </div>
+      <div className='aboutusheading'>
+        <h1>About Us</h1>
+      </div>
+      <div className='aboutuspara'>
+      We are proud to be the nationally recognized pizza brand. Starting from a mere neighborhood pizza delivery place in Karachi, we have now expanded to various areas of Pakistan including the metropolitan cities Karachi, Islamabad, and Multan, with seven kitchens in a matter of less than five years. We are the pioneers in introducing the New York style 20-Inch hand-tossed pizza in Pakistan which is customizable to every detail. We are the initiators of the 'One Slice is a Meal' concept - never before introduced in Pakistan. Our success lies in the unending devotion to keep delivering the biggest, yummiest pizza in town with the use of freshest toppings, the finest sauces and only the most delectable cheese on its New York Style hand-tossed pizzas, fuelled by the love, encouragement and feedback of our fans.
+      </div>
+      <div className='aboutuspizza'>
+      -------------------------------------
+        <ion-icon name='pizza-outline'></ion-icon>
+        -------------------------------------
+      </div>
     </div>
-  )
+  );
 }
 
-export default Aboutus
+export default Aboutus;
--- client/src/view/Speakyourmind.js
+++ client/src/view/Speakyourmind.js
@@ -1,14 +1,24 @@
-import React from 'react'
-import '../style/Speakyourmind.css'
+import React from 'react';
+import '../style/Speakyourmind.css';
+import speakyourmindbanner from '../images/Banners/speakyourmindbanner.jpg';
 
 function Speakyourmind() {
   return (
     <div>
-      <main>
-        Speak Your Mind
-      </main>
+      <div className='speakyourmindbanner'>
+        <img src={speakyourmindbanner} alt="speakyourmindbanner" />
+      </div>
+      <div className="speakyourmindform">
+        <form action="" id='formspeakyourmind'>
+          <input type='text' placeholder='Name' />
+          <input type='email' placeholder='Email' />
+          <input type='text' placeholder='City' />
+          <textarea name="" id="" cols="30" rows="10" form='formspeakyourmind' placeholder='Message'></textarea>
+          <button>SUBMIT</button>
+        </form>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Speakyourmind
+export default Speakyourmind;
