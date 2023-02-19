61ed84d7d92510da851155a80692ef40185f9a24
Huazifa-Ahmad <ahuzaifa64@gmail.com>
2023-02-20 00:11:08 +0500

    commit changes

--- client/src/view/Careers.js
+++ client/src/view/Careers.js
@@ -1,12 +1,26 @@
 import React from 'react'
 import '../style/Careers.css'
+import careersbanner from '../images/Banners/careersbanner.jpg'
 
 function Careers() {
   return (
     <div>
-      <main>
-        Careers
-      </main>
+      <div className='careersbanner'>
+        <img src={careersbanner} alt="careersbanner" />
+      </div>
+      <div className="wearenowhiringheading">
+        <h1>WE're Now Hiring</h1>
+      </div>
+      <div className='careersform'>
+        <form action="">
+        <input type='text' placeholder='Full Name' />
+        <input type='email' placeholder='Email' />
+        <input type='text' placeholder='City' />
+        <input type='text' placeholder='Department' />
+        <input type='file' placeholder='Attach CV' />
+        <button>SUBMIT</button>
+        </form>
+      </div>
     </div>
   )
 }
