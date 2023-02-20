ae213a758018e727d31d2221e7f8fe3fa1074a6c
Huazifa-Ahmad <ahuzaifa64@gmail.com>
2023-02-21 00:02:37 +0500

    commit changes

--- client/src/view/Contactus.js
+++ client/src/view/Contactus.js
@@ -1,12 +1,23 @@
 import React from 'react'
 import '../style/Contactus.css'
+import contactusbanner from '../images/Banners/contactusbanner.jpg'
 
 function Contactus() {
   return (
     <div>
-      <main>
-        Contact Us
-      </main>
+      <div className='contactusbanner'>
+        <img src={contactusbanner} alt="contactusbanner" />
+      </div>
+      <div className='contactusform'>
+        <form action="">
+        <input type='text' placeholder='Full Name' />
+        <input type='email' placeholder='Email' />
+        <input type='text' placeholder='Contact No.' />
+        <input type='text' placeholder='City' />
+        <textarea name="" id="" cols="30" rows="10" form='formspeakyourmind' placeholder='Message'></textarea>
+        <button>Send Message</button>
+        </form>
+      </div>
     </div>
   )
 }
