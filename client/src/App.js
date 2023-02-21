40e18a1507e295a2aaab779dbe6e0b54a7949745
Huazifa-Ahmad <ahuzaifa64@gmail.com>
2023-02-22 01:08:01 +0500

    commit changes

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,11 @@ import Contactus from './view/Contactus'
 import Careers from './view/Careers'
 import Speakyourmind from './view/Speakyourmind'
 import Aboutus from './view/Aboutus'
+import Pizza from './view/Pizza'
+import Sidelines from './view/Sidelines'
+import Desserts from './view/Desserts'
+import Drinks from './view/Drinks'
+import Dips from './view/Dips'
 import { Routes, Route } from 'react-router-dom'
 
 function App() {
@@ -20,6 +25,11 @@ function App() {
       <Route exact path='/careers' element={<Careers/>}/>
       <Route exact path='/Speak-your-mind' element={<Speakyourmind/>}/>
       <Route exact path='/about-us' element={<Aboutus/>}/>
+      <Route exact path='/pizza' element={<Pizza/>}/>
+      <Route exact path='/side-lines' element={<Sidelines/>}/>
+      <Route exact path='/desserts' element={<Desserts/>}/>
+      <Route exact path='/drinks' element={<Drinks/>}/>
+      <Route exact path='/dips' element={<Dips/>}/>
     </Routes>
     <Footer/>
     </>
--- client/src/view/Menu.js
+++ client/src/view/Menu.js
@@ -1,6 +1,12 @@
 import React from 'react'
 import '../style/Menu.css'
-//import { Link } from 'react-router-dom'
+import pizza from '../images/Menu/pizza.jpg'
+import dips from '../images/Menu/dips.jpg'
+import desserts from '../images/Menu/desserts.jpg'
+import drinks from '../images/Menu/drinks.jpg'
+import sidelines from '../images/Menu/sidelines.jpg'
+import deals from '../images/Menu/deals.jpg'
+import { Link } from 'react-router-dom'
 
 function Menu() {
   return (
@@ -10,12 +16,54 @@ function Menu() {
           <h1>Menu</h1>
         </div>
           <div className="menu-main">
-            <div className="menu-pizza">1</div>
-            <div className="menu-sidelines">2</div>
-            <div className="menu-desserts">3</div>
-            <div className="menu-deals">4</div>
-            <div className="menu-drinks">5</div>
-            <div className="menu-dips">6</div>
+            <div className="card">
+              <Link to="/pizza">
+                <img src={pizza} alt="" />
+              </Link>
+              <div className="label">
+                <h1>Pizza</h1>
+              </div>
+            </div>
+            <div className="card">
+              <Link to="/side-lines">
+                <img src={sidelines} alt="" />
+              </Link>
+              <div className="label">
+                <h1>Sidelines</h1>
+              </div>
+            </div>
+            <div className="card">
+              <Link to="/desserts">
+                <img src={desserts} alt="" />
+              </Link>
+              <div className="label">
+                <h1>Desserts</h1>
+              </div>
+            </div>
+            <div className="card">
+              <Link to="/deals">
+                <img src={deals} alt="" />
+              </Link>
+              <div className="label">
+                <h1>Deals</h1>
+              </div>
+            </div>
+            <div className="card">
+              <Link to="/drinks">
+                <img src={drinks} alt="" />
+              </Link>
+              <div className="label">
+                <h1>Drinks</h1>
+              </div>
+            </div>
+            <div className="card">
+              <Link to="/dips">
+                <img src={dips} alt="" />
+              </Link>
+              <div className="label">
+                <h1>Dips</h1>
+              </div>
+            </div>
           </div>
       </section>
     </div>
