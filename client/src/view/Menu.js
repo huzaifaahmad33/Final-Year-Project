bb9675ff86542db93a7e591a34406e638b30c8a6
Huazifa-Ahmad <ahuzaifa64@gmail.com>
2023-02-21 20:28:23 +0500

    commit changes

--- client/src/view/Menu.js
+++ client/src/view/Menu.js
@@ -1,12 +1,23 @@
 import React from 'react'
 import '../style/Menu.css'
+//import { Link } from 'react-router-dom'
 
 function Menu() {
   return (
     <div>
-      <main>
-        Menu
-      </main>
+      <section>
+        <div className='menu-heading'>
+          <h1>Menu</h1>
+        </div>
+          <div className="menu-main">
+            <div className="menu-pizza">1</div>
+            <div className="menu-sidelines">2</div>
+            <div className="menu-desserts">3</div>
+            <div className="menu-deals">4</div>
+            <div className="menu-drinks">5</div>
+            <div className="menu-dips">6</div>
+          </div>
+      </section>
     </div>
   )
 }
